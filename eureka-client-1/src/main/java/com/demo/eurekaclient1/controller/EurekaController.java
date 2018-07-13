c608340d62f652635ad53d313f75cb080b8416cf
guruss <guruss>
2018-07-13 15:35:45 +0530

    changes for zuul, hystrix

--- eureka-client-1/src/main/java/com/demo/eurekaclient1/controller/EurekaController.java
+++ eureka-client-1/src/main/java/com/demo/eurekaclient1/controller/EurekaController.java
@@ -1,6 +1,7 @@
 package com.demo.eurekaclient1.controller;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 @RequestMapping("/api/service/")
 @RefreshScope
@@ -26,12 +29,20 @@ public class EurekaController {
 	 String testValue;
 	
     @GetMapping("/names")
+    @HystrixCommand(fallbackMethod = "defaultMethod")
     public List<String> getUserName(){ 
        ResponseEntity<List<String>> result = restTemplate.exchange("http://client2/api/service/names", HttpMethod.GET,
                 null, new ParameterizedTypeReference<List<String>>() {});
 
         return result.getBody(); 
-//        return Arrays.asList("1","2");
     }
 
+    @GetMapping("/testValue")
+    public String getTestValue(){ 
+        return testValue; 
+    }
+    
+    public List<String> defaultMethod(){
+    	return Arrays.asList( "Hystrix Default Method");
+    }
 }
--- zuul-proxy-server/src/main/java/com/example/zuulproxyserver/ZuulProxyServerApplication.java
+++ zuul-proxy-server/src/main/java/com/example/zuulproxyserver/ZuulProxyServerApplication.java
@@ -1,16 +1,16 @@
-package com.demo.eurekaclient1;
+package com.example.zuulproxyserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableDiscoveryClient
-@EnableCircuitBreaker
 @SpringBootApplication
-public class EurekaClient1Application {
- 
+@EnableZuulProxy 
+@EnableDiscoveryClient
+public class ZuulProxyServerApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaClient1Application.class, args);
+		SpringApplication.run(ZuulProxyServerApplication.class, args);
 	}
 }
